ebcf5c70e5cd221535d1722a77b3a4e1f6a807d7
Jakub Klimas <jakub.wincent.klimas@gmail.com>
2021-11-09 20:39:17 +0100

    updated index.html and script.js

--- ForEach_Exercise/script.js
+++ ForEach_Exercise/script.js
@@ -1,19 +1,31 @@
 const li = document.querySelectorAll('li');
 const btnOne = document.querySelector('button');
 const btnTwo = document.querySelector('ul');
+
 let size = 10;
+let increase = 1;
 
 const adding = function () {
+	const liNew = document.createElement('li');
+	document.body.appendChild(liNew);
+	liNew.style.display = 'block';
+	liNew.style.fontSize = `${size}px`;
+	liNew.textContent = `element ${increase}`;
+	++increase;
 	++size;
 	li.forEach((li) => {
 		if (!li.style.display) {
 			li.style.display = 'block';
 		}
-		li.style.fontSize = `${size}px`;
 	});
 };
 const subtracting = function () {
+	const liNewDelete = document.querySelector('li');
+	liNewDelete.parentNode.removeChild(liNewDelete);
+	--increase;
 	--size;
+	liNew.style.fontSize = `${size}px`;
+	liNew.textContent = `element ${increase}`;
 	li.forEach((li) => {
 		if (!li.style.display) {
 			li.style.display = 'block';
