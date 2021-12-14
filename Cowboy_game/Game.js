70ce70d20fc8b1f40ae83d76e943876cd7858eab
Jakub Klimas <jakub.wincent.klimas@gmail.com>
2021-12-14 15:29:05 +0100

    Added new heroes avatars, updated shot() function

--- Cowboy_game/Game.js
+++ Cowboy_game/Game.js
@@ -2,14 +2,16 @@ class Game {
 	constructor() {
 		this.h1 = document.querySelector('h1');
 		this.h2 = document.querySelector('h2');
+		this.imgTwo = document.querySelector('img[id ="secound"]');
+		this.imgOne = document.querySelector('img[id ="first"]');
 		this.h1mid = document.querySelector('h1[id="midday"]');
 		this.h1Counting = document.querySelector('h1[id = "h1"]');
 		this.flag = false;
-		this.playeOneScore = 0;
+		this.playerOneScore = 0;
 		this.playerTwoScore = 0;
 		document.querySelector(
 			'h2[id ="score"]'
-		).textContent = `${this.playeOneScore} : ${this.playerTwoScore}`;
+		).textContent = `${this.playerOneScore} : ${this.playerTwoScore}`;
 		document
 			.querySelector('button')
 			.addEventListener('click', this.startGame.bind(this));
@@ -22,6 +24,10 @@ class Game {
 		let number = 3;
 
 		const counting = () => {
+			this.imgOne.src =
+				'https://freesvg.org/img/Cowboy-With-Guns-Standing-Wild-West-Vector-Pixel-Art.png';
+			this.imgTwo.src =
+				'./Cowboy-With-Guns-Standing-Wild-West-Vector-Pixel-Art.png';
 			this.h1mid.textContent = '';
 			if (number <= 0) {
 				clearInterval(interval);
@@ -31,7 +37,6 @@ class Game {
 			} else {
 				this.h1Counting.textContent = number;
 				number--;
-				console.log(number);
 			}
 		};
 		const interval = setInterval(counting, 1000);
@@ -39,7 +44,7 @@ class Game {
 	score(playerOne, playerTwo) {
 		document.querySelector(
 			'h2[id ="score"]'
-		).textContent = `${this.playeOneScore} : ${this.playerTwoScore}`;
+		).textContent = `${this.playerOneScore} : ${this.playerTwoScore}`;
 	}
 }
 
